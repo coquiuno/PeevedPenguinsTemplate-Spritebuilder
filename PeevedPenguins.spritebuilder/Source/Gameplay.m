fdaec61b03565188555db5cabb01d7bb9540fd7d
Compu <Coquiuno@Caminante.local>
2014-11-15 17:30:09 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -13,7 +13,7 @@
     CCPhysicsNode *_physicsNode;
     CCNode *_catapultArm;
     CCNode *_levelNode;
-    //CCNode *_contentNode;
+    CCNode *_contentNode;
 }
 
 // is called when CCB file has completed loading
@@ -53,7 +53,7 @@
     
     self.position = ccp(0,0);
     CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
-    [self runAction:follow];
+    [_contentNode runAction:follow];
     
 }
 
